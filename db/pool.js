3fc3c2181ec9a4f3dd8c725453c8784e24aec729
Hilal Arsa <hilalarsa@gmail.com>
2020-06-28 16:00:48 +0700

    Add script folder inside server to be deployed

--- db/pool.js
+++ db/pool.js
@@ -1,19 +1,19 @@
 const { Client, Pool } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-client.connect();
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'tagger',
-//   password: 'password',
-//   port: 5432,
-// })
+// client.connect();
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'tagger',
+  password: 'password',
+  port: 5432,
+})
 
-module.exports = client;
+module.exports = pool;
